90d780b0bb0d6c9704a3593632f10756892bd1cc
chris <chrissx@chrissx.de>
2021-08-03 16:15:54 +0200

    Many improvements.
    - Nicer code style
    - It's now an `img` tag so it can be saved in most browsers.
    - It uses now `fonts.chrissx.de`
    - Dark mode by default and as only option

--- main.js
+++ main.js
@@ -1,38 +1,42 @@
-'use strict';
-
 function make() {
-        var output = document.getElementById('output');
+        var canvas = document.getElementById('canvas');
+        var imgout = document.getElementById('out');
         var inputFile = document.getElementById('rawimage').files[0];
         var upper = document.getElementById('upper').value.toUpperCase();
         var lower = document.getElementById('lower').value.toUpperCase();
         var reader = new FileReader();
-        reader.onload = function() {
+        reader.onload = async function() {
                 var dataURL = reader.result;
                 var src = new Image();
                 src.src = dataURL;
-                src.decode().then(function() {
-                        output.setAttribute('width', src.width);
-                        output.setAttribute('height', src.height);
-                        var ctx = output.getContext('2d');
-                        ctx.drawImage(src, 0, 0);
-                        ctx.font = '60px mimpact';
-                        ctx.textAlign = 'center';
-                        ctx.shadowColor = 'white';
-                        ctx.shadowOffsetX = -5;
-                        ctx.shadowOffsetY = -5;
-                        ctx.fillText(upper, src.width / 2, 50);
-                        ctx.fillText(lower, src.width / 2, src.height - 20);
-                        ctx.shadowOffsetX = 5;
-                        ctx.fillText(upper, src.width / 2, 50);
-                        ctx.fillText(lower, src.width / 2, src.height - 20);
-                        ctx.shadowOffsetX = -5;
-                        ctx.shadowOffsetY = 5;
-                        ctx.fillText(upper, src.width / 2, 50);
-                        ctx.fillText(lower, src.width / 2, src.height - 20);
-                        ctx.shadowOffsetX = 5;
-                        ctx.fillText(upper, src.width / 2, 50);
-                        ctx.fillText(lower, src.width / 2, src.height - 20);
-                });
+                await src.decode();
+
+                canvas.setAttribute('width', src.width);
+                canvas.setAttribute('height', src.height);
+                var ctx = canvas.getContext('2d');
+
+                ctx.drawImage(src, 0, 0);
+                ctx.font = '60px Impact';
+                ctx.textAlign = 'center';
+                ctx.shadowColor = 'white';
+
+                ctx.shadowOffsetX = -5;
+                ctx.shadowOffsetY = -5;
+                ctx.fillText(upper, src.width / 2, 50);
+                ctx.fillText(lower, src.width / 2, src.height - 20);
+                ctx.shadowOffsetX = 5;
+                ctx.fillText(upper, src.width / 2, 50);
+                ctx.fillText(lower, src.width / 2, src.height - 20);
+                ctx.shadowOffsetX = -5;
+                ctx.shadowOffsetY = 5;
+                ctx.fillText(upper, src.width / 2, 50);
+                ctx.fillText(lower, src.width / 2, src.height - 20);
+                ctx.shadowOffsetX = 5;
+                ctx.fillText(upper, src.width / 2, 50);
+                ctx.fillText(lower, src.width / 2, src.height - 20);
+
+                imgout.src = canvas.toDataURL();
+                canvas.width = canvas.height = 0;
         };
         if(inputFile) {
                 reader.readAsDataURL(inputFile);
